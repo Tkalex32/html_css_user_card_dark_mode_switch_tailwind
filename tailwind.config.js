4b0e8bf45c9937bc9393799aa2e48d21a17a0416
Tkalex32 <tkalexsc@gmail.com>
2021-12-09 08:25:36 +0100

    fix: finalize css and js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
     extend: {
       fontFamily: {
         poppins: ["Poppins"],
+      },
+      boxShadow: {
+        card: "0 15px 25px rgba(0, 0, 0, 0.025)"
+      },
+      fontSize: {
+        imgtext: ["1.4em", "1.2em"],
+        h3: "0.75em"
       }
     },
   },
